99aca5f500e0db4e1e3b862dccd75a7812459d84
laggingreflex <laggingreflex@gmail.com>
2018-02-22 05:06:57 +0530

    feat: New `hyperchain/text` helper to render to string

---
+++ text.js
@@ -0,0 +1,22 @@
+const hyperchain = require('.');
+
+module.exports = opts => hyperchain(toString, opts);
+
+function toString(tag, attrs, children) {
+  return `<${tag}`
+    + Object.keys(attrs)
+    .map(attr => {
+      const val = attrs[attr];
+      switch (typeof val) {
+        case 'boolean':
+        case 'number':
+        case 'string':
+          return ` ${attr}=${JSON.stringify(val)}`;
+      }
+    })
+    .filter(Boolean)
+    .join('')
+    + `>`
+    + children.join('')
+    + `</${tag}>`;
+}
